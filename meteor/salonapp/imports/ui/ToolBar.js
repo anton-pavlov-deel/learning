cd3c50b0e0104e8170d028f842583e5241051de9
pavlovanton <panton.anton98@gmail.com>
2018-02-23 15:09:02 +0300

    WIP add note widget

--- meteor/salonapp/imports/ui/ToolBar.js
+++ meteor/salonapp/imports/ui/ToolBar.js
@@ -7,6 +7,7 @@ export default class ToolBar extends Component {
     return (
       <div id='tool-bar-div'>
         <SearchBar onFilterChange={this.props.onFilterChange}/>
+        <AddNoteTool />
       </div>
     );
   }
@@ -52,6 +53,41 @@ class SearchBar extends Component {
           onChange={this.handleFilterChange.bind(this)}
         /></label>
       </div>
+    );
+  }
+}
+
+class AddNoteTool extends Component {
+  render() {
+    return (
+      <form id="add-note-tool-form">
+        <h3>Add note tool</h3>
+        <label>Service: <input
+          type='text'
+          name='newService'
+        /></label><br />
+        <label>Provided by: <input
+          type='text'
+          name='newProvidedBy'
+        /></label><br />
+        <label>Provided to: <input
+          type='text'
+          name='newProvidedTo'
+        /></label><br />
+        <label>Clear price: <input
+          type='text'
+          name='newClearPrice'
+        /></label><br />
+        <label>Price: <input
+          type='text'
+          name='newPrice'
+        /></label><br />
+        <label>Date: <input
+          type='text'
+          name='newDate'
+        /></label>
+        <input type='submit' name='Submit' value='Add note'/>
+      </form>
     );
   }
 }
